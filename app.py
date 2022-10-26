667c60c5b1d97005ca58616304c95f16305b7d5f
Matt <61901583+zewas1@users.noreply.github.com>
2022-10-26 19:25:47 +0700

    template rendering

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -9,13 +8,16 @@ DEBUG = True
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {} </p>'.format(user_agent)
+    return render_template('index.html')
 
 
 @app.route('/user')
 def user():
     name = request.args.get('name')
-    if not name:
-        return '<h1>Bad Request</h1>', 400
-    return '<h1>Hello, {}!</h1>'.format(name), 200
+    # if not name:
+    #     return '<h1>Bad Request</h1>', 400
+
+    list = ['one', 'two', 'three', 'four']
+
+    return render_template('user.html', name=name, list=list)
 
